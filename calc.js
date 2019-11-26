7b2c5e1bc4f2834a03eb0b82bc8d7e863cf3d1a4
cgardn <christopher.gardn@gmail.com>
2019-11-26 15:10:23 -0500

    numbers input and display correctly with minor spacing issues, no actual operating yet

--- calc.js
+++ calc.js
@@ -1,9 +1,118 @@
+const Display = {
+    
+    updateDisplay,
+    
+    update: () => {
+        let displayContent = '';
+        if (firstNum) displayContent += firstNum;
+        if (operator) displayContent += ` ${operator} `;
+        if (secondNum) displayContent += secondNum;
+        display.textContent = displayContent;
+    },
+
+    clear: () => {
+        displayContent = '';
+        display.textContent = '';
+    }
+}
+
+//  design intent: the only place data is stores is numbers[], operators[], and 
+//    lastResult
+//      all operations actually happen on the data, display functions only read
+
+let numbers = [];
+let operators = [];
+let workingNumber = '';
+let lastResult = 0;
+
+const display = document.querySelector('#calc-display');
+
+const Actions = {
+    add,subtract,multiply,divide,operate,
+    clear,
+}
+
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function operate(a,b,opr) {
+    // FIXME
+    switch (opr) {
+        case 'add': return add(a,b);
+        case 'subtract': return subtract(a,b);
+        case 'multiply': return multiply(a,b);
+        case 'divide': return divide(a,b);
+    }
+}
+
+function updateDisplay() {
+    display.textContent = " " + ` ${numbers.join(' ')} ${workingNumber}` + ' ';
+}
+
+function clear() {
+    numbers = [];
+    operators = [];
+    workingNumber = '';
+}
+
+function clickNum(newChar) {
+    if (workingNumber.includes(newChar) && newChar == '.') return;
+    else {
+        workingNumber += newChar;
+    }
+}
+
+function clickOp(newChar) {
+    const ops = '/*-+';
+
+    if (!ops.includes(workingNumber)) {
+        numbers.push(workingNumber);
+    }
+    workingNumber = newChar;
+}
+
+function doAction(action) {
+    Actions[action]();
+}
+
+function handleButton(char, e) {
+
+    let newChar = e.target.textContent;
+    let action = e.target.getAttribute('data-key');
+
+    const nums = '0123456789.'
+    const ops = '/*+-';
+    const funcs = '<C=';
+
+    if (nums.includes(newChar)) clickNum(newChar);
+    else if (ops.includes(newChar)) clickOp(newChar);
+    else if (funcs.includes(newChar)) doAction(action);
+
+    updateDisplay();
+}
+
+// --> DOC SETUP CODE FOLLOWS <-- //
 function placeButtons() {
     let btns = Array.from(document.querySelectorAll('.button-num'));
     
     btns.map( btn => {
         btn.style.gridArea = btn.getAttribute('data-key');
+        btn.addEventListener('click', (e) => handleButton(btn.textContent, e));
     });
 }
 
 placeButtons();
+// --> END STYLING CODE <-- //
